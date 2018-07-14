9b2c28c0a7af0fdf1f41c5495f3e3cbd5d8c29c0
llr392425595 <392425595@qq.com>
2018-07-14 14:00:32 +0800

    [LLChain] add a simple POW algorithm

--- block_chain.py
+++ block_chain.py
@@ -1,5 +1,5 @@
-import json
 import hashlib
+import json
 
 
 class BlockChain(object):
@@ -27,3 +27,16 @@ class BlockChain(object):
     @property
     def last_block(self):
         return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f"{last_proof}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == '0000'
+
