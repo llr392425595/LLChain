c74e7df3b7d0d7cd950d05ba9667872f0a0b0ca9
Liang <392425595@qq.com>
2018-07-17 19:19:15 +0800

    [LLChian]extract model and service

--- model/block.py
+++ model/block.py
@@ -1,17 +1,17 @@
 import hashlib
 from time import time
 
-from flask import json
-
 
 class Block:
     def __init__(self, block_header, block_body):
         self.block_header = block_header
         self.block_body = block_body
 
-    def hash(self):
-        block_string = json.dumps(self, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
+    def hash_block(self):
+        return hashlib.sha256(self.str_block().encode()).hexdigest()
+
+    def str_block(self):
+        return self.block_header.str_block_header() + self.block_body.str_block_body()
 
 
 class BlockHeader:
@@ -21,9 +21,13 @@ class BlockHeader:
         self.proof = proof
         self.previous_hash = previous_hash
 
+    def str_block_header(self):
+        return str(self.index) + str(self.timestamp) + str(self.previous_hash) + str(self.proof)
+
 
 class BlockBody:
     def __init__(self, transactions=None):
-        if transactions is None:
-            transactions = []
-        self.index = transactions
+        self.transactions = transactions
+
+    def str_block_body(self):
+        return str(self.transactions)
---
+++ model/proof_generator.py
@@ -0,0 +1,15 @@
+import hashlib
+
+
+class ProofGenerator:
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f"{last_proof}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == '0000'
