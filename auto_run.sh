a41cd607f2a198208f6f4a466e9b858191799a10
Liang <392425595@qq.com>
2018-07-18 22:04:47 +0800

    [LLChian]  set flask app enter & update auto_run

--- auto_run.sh
+++ auto_run.sh
@@ -3,3 +3,6 @@
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
+export FLASK_APP="main.py"
+
+flask run
--- main.py
+++ main.py
@@ -99,4 +99,11 @@ def consensus():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=8000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='127.0.0.1', port=port)
